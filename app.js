10dd3049447696202439233d3b2bca389bb5b194
ehedei <ehedeidev@gmail.com>
2021-10-03 19:52:34 +0100

    Menu: Added exit mechanics

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require('colors')
-const { showMenu } = require('./helpers/messages')
+const { showMenu, pause } = require('./helpers/messages')
 
 const main = async () => {
-  showMenu()
+  let option = ''
+
+  do {
+    option = await showMenu()
+    if (option !== '0') await pause()
+  } while (option !== '0')
+
+  console.log('\nGoodbye!\n'.green)
 }
 
 console.clear()
